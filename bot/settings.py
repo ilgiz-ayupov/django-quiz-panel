3cd99d7333dd1ba50abcb61f66f076b095a10f34
ilgiz <ilgizayupov7@gmail.com>
2021-10-07 14:59:34 +0500

    Поставил косую черту

--- bot/settings.py
+++ bot/settings.py
@@ -2,5 +2,5 @@ BOT_TOKEN = "2025046509:AAFb52T3xQNd1fv3Md2oZUD1BiMme2JtQLI"
 
 # webhook settings
 WEBHOOK_HOST = "https://django-quiz-panel.herokuapp.com"
-WEBHOOK_PATH = f"/webhook{BOT_TOKEN}"
+WEBHOOK_PATH = f"/webhook{BOT_TOKEN}/"
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
